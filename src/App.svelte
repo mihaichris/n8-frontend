9c66c11ab1a59728c9ee9632d072cc7bc06ce573
Mihai <mihai-cristian.fagadau@my.fmi.unibuc.ro>
2020-05-30 23:18:32 +0300

    add style, basic search bar

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,24 @@
 <script>
-	export let name;
+  import TailwindCss from "./TailwindCss.svelte";
+  import AppCss from "./style/AppCss.svelte";
+  export let name;
 </script>
 
+<TailwindCss />
+<AppCss />
+
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+  <div id="cover">
+  <form method="get" action="">
+    <div class="tb">
+      <div class="td"><input type="text" placeholder="Search" required></div>
+      <div class="td" id="s-cover">
+        <button type="submit">
+          <div id="s-circle"></div>
+          <span></span>
+        </button>
+      </div>
+    </div>
+  </form>
+</div>
 </main>
-
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>

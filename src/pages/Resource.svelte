139b814c862d58dea052c9d0822d299e2929d76c
Mihai <48624094+mihaichris@users.noreply.github.com>
2021-05-03 23:12:33 +0300

    resource view preparing for data income

--- src/pages/Resource.svelte
+++ src/pages/Resource.svelte
@@ -6,17 +6,24 @@
     import {RESOURCE_REQUEST, SEARCH_REQUEST} from "../models/Request";
 
     export let id;
-    export let resource;
+    export let ontologyProperties = [];
+    export let ontologyRelations = [];
     onMount(() => {
-        console.log(id);
         findEntityBySubjectUri(id).then((response) => {
-            resource = response.data;
+            response.data.ontologyProperties.forEach((ontologyProperty) => {
+                if (ontologyProperty["@id"].includes(id)) {
+                    ontologyProperties.push(ontologyProperty);
+                } else {
+                    ontologyRelations.push(ontologyProperty);
+                }
+            });
+            console.log(ontologyProperties);
         });
     });
 
     const findEntityBySubjectUri = async (subjectUri) => {
         try {
-            return await axios.get(RESOURCE_REQUEST + "/" + subjectUri);
+            return await axios.post(RESOURCE_REQUEST, {uri: subjectUri});
         } catch (error) {
             console.log(error);
         }
@@ -25,8 +32,26 @@
 </script>
 <section class="container mx-auto px-8">
     <div class="min-w-screen min-h-screen bg-yellow-300 flex items-center p-5 lg:p-10 overflow-hidden relative">
-        <div class="w-full max-w-6xl rounded bg-white shadow-xl p-10 lg:p-20 mx-auto text-gray-800 relative md:text-left">
+        <div class="w-full max-w-6xl rounded p-10 lg:p-20 mx-auto text-gray-800 relative md:text-left">
+            <h3 class="text-center font-bold text-2xl font-sans  text-yellow-700">{id}</h3> &nbsp;
+            <div class="w-full max-w-6xl rounded bg-white shadow-xl  p-10 lg:p-20 mx-auto text-gray-800 relative md:text-left">
+                <h3 class="text-center font-bold text-lg font-sans ">Ontology Description</h3> &nbsp;
+            </div>
         </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
         <div class="flex items-end justify-end fixed bottom-0 right-0 mb-4 mr-4 z-10">
